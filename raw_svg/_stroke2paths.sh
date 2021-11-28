c91baa56d9374ff11c3e9145b53a0293df8e2387
H. Árkosi Róbert <robreg@zsurob.hu>
2021-11-29 00:28:44 +0100

    Update _stroke2paths.sh

--- raw_svg/_stroke2paths.sh
+++ raw_svg/_stroke2paths.sh
@@ -1,3 +1,2 @@
-#find ./ -type f -name "*.svg" -exec sed -i 's/#FFFFFF/#000000/g' {} \;
 for file in *.svg; do inkscape --batch-process --with-gui --actions "EditSelectAll;SelectionUnGroup;EditSelectAll;StrokeToPath;FileSave;FileQuit" "$file";done
 
